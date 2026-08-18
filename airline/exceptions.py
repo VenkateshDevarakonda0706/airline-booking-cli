725b01f4099f5a92534f8a1fad8f0fb1bf02e30d
VenkateshDevarakonda0706 <venkateshdevarakonda0706@gmail.com>
2026-08-18 20:13:33 +0530

    feat: add airline booking exception hierarchy

---
+++ airline/exceptions.py
@@ -0,0 +1,30 @@
+class AirlineBookingError(Exception):
+    """Base exception for the airline booking application."""
+
+
+class FlightNotFoundError(AirlineBookingError):
+    """Raised when a requested flight cannot be found."""
+
+
+class PassengerValidationError(AirlineBookingError):
+    """Raised when passenger information is invalid."""
+
+
+class SeatUnavailableError(AirlineBookingError):
+    """Raised when a requested seat is unavailable."""
+
+
+class NoAvailableSeatsError(AirlineBookingError):
+    """Raised when a flight has no available seats."""
+
+
+class BookingNotFoundError(AirlineBookingError):
+    """Raised when a booking cannot be found."""
+
+
+class BookingCancellationError(AirlineBookingError):
+    """Raised when a booking cannot be cancelled."""
+
+
+class InvalidBookingError(AirlineBookingError):
+    """Raised when a booking operation contains invalid data."""
